0372c3256760dbd47fac321eaa4966e867ee6d7e
Rob van der Leek <robvanderleek@gmail.com>
2019-11-26 20:34:26 +0100

    Minimal failing example

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,24 +1,60 @@
 const Octokit = require('@octokit/rest');
 const nock = require('nock');
+const {Probot} = require('probot');
+const checkSuitePayload = require('./check_suite.requested')
 
 nock.disableNetConnect();
 
-beforeEach(() => {
-   nock.cleanAll();
-   jest.setTimeout(10000);
-   nockAccessToken();
+test('1. correctly shows missing network mock', async () => {
+    nock('https://api.github.com').get('/some/endpoint').reply(404)
+
+    const octokit = Octokit();
+    const user = await octokit.users.getByUsername({
+        username: 'robvanderleek'
+    });
+    console.log(user);
 });
 
-function nockAccessToken () {
-   nock('https://api.github.com')
-       .post('/app/installations/1296032/access_tokens')
-       .reply(200, { token: 'test' });
-}
+test('2. also works correctly due to no missing network mock', async () => {
+    const probot = new Probot({});
+    const probotApp = (app) => {
+        app.on(['check_suite.requested', 'check_run.rerequested'], check)
 
-test('hanging nock request', async () => {
-   const octokit = Octokit();
-   const user = await octokit.users.getByUsername({
-      username: 'robvanderleek'
-   });
-   console.log(user);
+        async function check(context) {
+            await context.config('issue-branch.yml', {})
+        }
+    };
+    const app = probot.load(probotApp);
+    app.app = {
+        getInstallationAccessToken: () => Promise.resolve('test')
+    };
+
+    nock('https://api.github.com')
+        .get('/repos/hiimbex/testing-things/contents/.github/issue-branch.yml')
+        .reply(404)
+        .get('/repos/hiimbex/.github/contents/.github/issue-branch.yml')
+        .reply(404);
+
+    await probot.receive({name: 'check_suite', payload: checkSuitePayload});
+});
+
+test('3. Async timeout due to missing network mock', async () => {
+    const probot = new Probot({});
+    const probotApp = (app) => {
+        app.on(['check_suite.requested', 'check_run.rerequested'], check)
+
+        async function check(context) {
+            await context.config('issue-branch.yml', {})
+        }
+    };
+    const app = probot.load(probotApp);
+    app.app = {
+        getInstallationAccessToken: () => Promise.resolve('test')
+    };
+
+    nock('https://api.github.com')
+        .get('/repos/hiimbex/testing-things/contents/.github/issue-branch.yml')
+        .reply(404);
+
+    await probot.receive({name: 'check_suite', payload: checkSuitePayload})
 });
